b90be7c3c068a2c5afc9a6d53b9cf6725c5efc8a
lukaszplac123 <lukasz.plac@gmail.com>
2017-08-30 22:10:49 +0200

    Next release. create function outside Button()

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $(function() {
 	function Button(text) {
 		this.text = text || "Hello"
-		//Button.prototype = {  ten sposob u mnie nie dziala, a tak to jest opisane w module !?, dopiero ponizszy sposob dziala 
-			//create: function(){
-		Button.prototype.create = function() {
+	}
+
+	Button.prototype = {
+		create : function() {
 				var self = this;
 				this.$element = $('<button>');
 				this.$element.text(this.text);
@@ -11,7 +12,7 @@ $(function() {
 					alert(self.text);
 				});
 				$('body').append(this.$element);
-			}
+		}
 	}
 	
 	var btn1 = new Button("Hello!");
